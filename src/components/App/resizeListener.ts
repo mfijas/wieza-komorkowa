ee415900f184cc20a9db73c3eae47077326cd7ab
mfijas <michal.fijas@gmail.com>
2022-07-17 23:26:11 +0200

    Fix to font resizing code

--- src/components/App/resizeListener.ts
+++ src/components/App/resizeListener.ts
@@ -1,15 +1,18 @@
 export default function addResizeListener() {
     function getCssRule(selectorText: string) {
-        for (const cssRule of document.styleSheets[1].cssRules) {
-            const cssStyleRule = cssRule as CSSStyleRule;
-            if (cssStyleRule.selectorText === selectorText) {
-                return cssStyleRule;
+        for (const styleSheet of document.styleSheets) {
+            for (const cssRule of styleSheet.cssRules) {
+                const cssStyleRule = cssRule as CSSStyleRule;
+                if (cssStyleRule.selectorText === selectorText) {
+                    return cssStyleRule;
+                }
             }
         }
         throw new Error(`Rule for ${selectorText} not found!`);
     }
 
     const cssRules = ['body', 'button'].map(elem => getCssRule(elem));
+    console.log(cssRules);
 
     function resizeFont() {
         const fontSize = window.innerHeight * 0.0357;
