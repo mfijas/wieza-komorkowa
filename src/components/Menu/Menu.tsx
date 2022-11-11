4062673d46a927dafd5a2b03faf93d6fa79e2223
mfijas <michal.fijas@gmail.com>
2022-11-11 17:53:49 +0100

    Added new game functionality

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,11 @@
-export function Menu() {
-    return (
-        <div>Menu</div>
-    );
+interface MenuParams {
+  newGame: () => void;
+}
+
+export function Menu({ newGame }: MenuParams) {
+  return (
+    <div>
+      <button onClick={() => newGame()} style={{ width: '100%', height: '2em' }}>Nowa gra</button>
+    </div>
+  );
 }
--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,74 +1,79 @@
 import {
-    checkIfPuzzleCompleted,
-    checkIfSelectionIsContiguous,
-    extractSelectedWord,
-    getNumberOfSelectedTiles
+  checkIfPuzzleCompleted,
+  checkIfSelectionIsContiguous,
+  extractSelectedWord,
+  getNumberOfSelectedTiles
 } from '../../puzzle/matrixFunctions';
 import { allWords } from '../../puzzle/words';
 import { TileState } from '../App/tileState';
 import './Status.scss';
 
 interface StatusParams {
-    matrix: string[][];
-    tileState: TileState[][];
-    markWord: () => void;
+  matrix: string[][];
+  tileState: TileState[][];
+  markWord: () => void;
+  newGame: () => void;
 }
 
-export function Status({ matrix, tileState, markWord }: StatusParams) {
-    const selectedWord = extractSelectedWord(matrix, tileState);
-    const isProperWord = (selectedWord: string) => allWords.indexOf(selectedWord) !== -1;
+export function Status({ matrix, tileState, markWord, newGame }: StatusParams) {
+  const selectedWord = extractSelectedWord(matrix, tileState);
+  const isProperWord = (selectedWord: string) => allWords.indexOf(selectedWord) !== -1;
 
-    function onClick() {
-        if (isProperWord(selectedWord)) {
-            markWord();
-        }
-    }
-
-    let message: string;
-    let disabled: boolean;
-    if (checkIfPuzzleCompleted(tileState)) {
-        message = 'Gratulacje, gotowe!';
-        disabled = true;
-    } else if (getNumberOfSelectedTiles(tileState) === 0) {
-        message = 'Zaznacz słowo';
-        disabled = true;
-    } else if (checkIfSelectionIsContiguous(tileState)) {
-        if (selectedWord.length < 4) {
-            message = 'Za krótkie: ' + selectedWord.toUpperCase();
-            disabled = true;
-        } else if (selectedWord.length > 8) {
-            message = 'Za długie słowo!';
-            disabled = true;
-        } else {
-            if (isProperWord(selectedWord)) {
-                message = 'Dodaj słowo: ' + selectedWord.toUpperCase();
-                disabled = false;
-            } else {
-                message = 'To nie słowo: ' + selectedWord.toUpperCase();
-                disabled = true;
-            }
-        }
+  function onClick() {
+    if (!checkIfPuzzleCompleted(tileState)) {
+      if (isProperWord(selectedWord)) {
+        markWord();
+      }
     } else {
-        message = 'Obszar musi być ciągły';
-        disabled = true;
+      newGame();
     }
+  }
 
-    const margin = 1;
-    return (
-        <div className="status">
-            <button onClick={() => onClick()} style={{ width: '100%', height: '2em' }} disabled={disabled}>
-                <svg viewBox="0 0 100 14">
-                    <rect
-                        x={margin}
-                        y={margin}
-                        width={100 - 2 * margin}
-                        height={14 - 2 * margin}
-                        rx={margin}
-                        ry={margin}
-                    />
-                    <text x="50%" y="50%">{message}</text>
-                </svg>
-            </button>
-        </div>
-    );
+  let message: string;
+  let disabled: boolean;
+  if (checkIfPuzzleCompleted(tileState)) {
+    message = 'Gratulacje, gotowe!';
+    disabled = false;
+  } else if (getNumberOfSelectedTiles(tileState) === 0) {
+    message = 'Zaznacz słowo';
+    disabled = true;
+  } else if (checkIfSelectionIsContiguous(tileState)) {
+    if (selectedWord.length < 4) {
+      message = 'Za krótkie: ' + selectedWord.toUpperCase();
+      disabled = true;
+    } else if (selectedWord.length > 8) {
+      message = 'Za długie słowo!';
+      disabled = true;
+    } else {
+      if (isProperWord(selectedWord)) {
+        message = 'Dodaj słowo: ' + selectedWord.toUpperCase();
+        disabled = false;
+      } else {
+        message = 'To nie słowo: ' + selectedWord.toUpperCase();
+        disabled = true;
+      }
+    }
+  } else {
+    message = 'Obszar musi być ciągły';
+    disabled = true;
+  }
+
+  const margin = 1;
+  return (
+    <div className="status">
+      <button onClick={() => onClick()} style={{ width: '100%', height: '2em' }} disabled={disabled}>
+        <svg viewBox="0 0 100 14">
+          <rect
+            x={margin}
+            y={margin}
+            width={100 - 2 * margin}
+            height={14 - 2 * margin}
+            rx={margin}
+            ry={margin}
+          />
+          <text x="50%" y="50%">{message}</text>
+        </svg>
+      </button>
+    </div>
+  );
 }
